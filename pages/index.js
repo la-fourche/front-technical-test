51efeb5a6d6abc0bed6f6e2f7463868e91ebdeca
Julien Thibeaut <julien.thibeaut@gmail.com>
2020-02-26 18:49:39 +0100

    update index

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,17 @@ const Index = () => {
     <Layout>
       <h1>Julien Thibeaut - Test technique</h1>
       <p>Réalisé en ≃3h30</p>
+      <p>
+        Comme demandé, on a une app comprenant une page collection de produits,
+        paginée par 20, ainsi qu'une page fiche produit.
+      </p>
+      <p>Sans la contrainte de temps j'aurais pu:</p>
+      <ul>
+        <li>Utiliser Typescript</li>
+        <li>Appronfondir du côté PWA</li>
+        <li>Ajouter du style</li>
+        <li>Factoriser du code/créer des components</li>
+      </ul>
     </Layout>
   );
 };
